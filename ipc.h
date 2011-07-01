2c4f1cac1af55d2061ffac568f165e7151fd8197
Jules Blok <jules.blok@gmail.com>
2011-07-01 18:07:33 +0000

    Only include IPC header once
    git-svn-id: http://jules.aerix.nl/svn/g-key/branches/g930@55 f5aa9b1c-f3d2-43f6-b11e-4419a1f85e2c

--- ipc.h
+++ ipc.h
@@ -1,3 +1,6 @@
+#ifndef GKEY_IPC
+#define GKEY_IPC
+
 #include <Windows.h>
 
 #define IPC_BUFSIZE 256
@@ -11,3 +14,5 @@ BOOL IpcInit(void);
 BOOL IpcRead(IpcMessage* message, DWORD dwMilliseconds);
 BOOL IpcWrite(IpcMessage* message, DWORD dwMilliseconds);
 BOOL IpcClose(void);
+
+#endif
