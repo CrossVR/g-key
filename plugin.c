d79d3d58a30c224a7763dc2b354887d8cc55f8db
Jules Blok <jules.blok@gmail.com>
2011-07-03 23:39:21 +0000

    git-svn-id: http://jules.aerix.nl/svn/g-key/trunk@67 f5aa9b1c-f3d2-43f6-b11e-4419a1f85e2c

--- plugin.c
+++ plugin.c
@@ -384,7 +384,7 @@ DWORD WINAPI DebugThread(LPVOID pData)
 				if(DebugActiveProcess(ProcessId)) DebugMain(ProcessId, hProcess);
 				else 
 				{
-					// Could not attach debugger, exit plugin
+					// Could not attach debugger, exit debug thread
 					ts3Functions.logMessage("Failed to attach debugger, are you using the correct version for your platform?", LogLevel_ERROR, "G-Key Plugin", 0);
 					return 1;
 				}
