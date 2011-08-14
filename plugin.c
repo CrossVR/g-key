88c10f393f8ee69786abfd4f0c07c237cf2fbc5c
Jules Blok <jules.blok@gmail.com>
2011-08-14 12:37:42 +0000

    Updated version number
    git-svn-id: http://jules.aerix.nl/svn/g-key/trunk@95 f5aa9b1c-f3d2-43f6-b11e-4419a1f85e2c

--- plugin.c
+++ plugin.c
@@ -469,7 +469,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.4";
+    return "0.4.1";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
