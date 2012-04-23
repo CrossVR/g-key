d16e69fa4859dd63261b090a4608f517ffb568dd
Jules Blok <jules.blok@gmail.com>
2012-04-23 16:27:45 +0000

    Updated TeamSpeak copyright
    git-svn-id: http://jules.aerix.nl/svn/g-key/trunk@160 f5aa9b1c-f3d2-43f6-b11e-4419a1f85e2c

--- functions.c
+++ functions.c
@@ -2,7 +2,7 @@
  * TeamSpeak 3 G-key plugin
  * Author: Jules Blok (jules@aerix.nl)
  *
- * Copyright (c) 2008-2011 TeamSpeak Systems GmbH
+ * Copyright (c) 2008-2012 TeamSpeak Systems GmbH
  */
 
 #include <Windows.h>
