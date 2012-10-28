8d60edd5835cb41d4af2ae49ecb0b07bf0f664d7
Jules Blok <jules.blok@gmail.com>
2012-10-28 17:32:41 +0000

    Increment version number
    git-svn-id: http://jules.aerix.nl/svn/g-key/trunk@290 f5aa9b1c-f3d2-43f6-b11e-4419a1f85e2c

--- plugin.cpp
+++ plugin.cpp
@@ -795,7 +795,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.5.8";
+    return "0.5.9";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
